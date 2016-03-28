60204f7d6a805be013e76b91d186eed6fab2b8fd
Ed Johnstone <johnedstone@gmail.com>
2016-03-28 03:46:53 +0000

    working on picha

--- picha/urls.py
+++ picha/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from feedback.views import FeedbackView
 
 urlpatterns = [
-    url(r'^$', 'welcome.views.index'),
+    url(r'^$', 'welcome.views.index', name='home'),
     url(r'^health$', 'welcome.views.health'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^feedback/$', FeedbackView.as_view(), name="feedback"),
+    url(r'^feedback/$', FeedbackView.as_view(), name='feedback'),
 ]
