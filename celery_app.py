8b6f4f396cadfed02aec0cefdc2307cb33e526ed
Ed Johnstone <johnedstone@gmail.com>
2016-04-04 06:34:34 +0000

    working on templates

--- celery_app.py
+++ celery_app.py
@@ -1,8 +1,8 @@
 import shlex, subprocess, sys
 from subprocess import PIPE
 args = shlex.split(''' echo celery worker -A picha.celery -B -Q default -n default@%h -l info  ''')
-# stdout, stderr = subprocess.Popen(args, stdout=PIPE).communicate()
-try:
-    p = subprocess.Popen(args)
-except Exception as err:
-    print("Error: {0}".format(err))
+stdout, stderr = subprocess.Popen(args, stdout=PIPE).communicate()
+#try:
+#    p = subprocess.Popen(args)
+#except Exception as err:
+#    print("Error: {0}".format(err))
