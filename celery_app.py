5628d70548d578f3798f07c3afb0fd9937e84234
Ed Johnstone <johnedstone@gmail.com>
2016-04-04 05:43:06 +0000

    trying to get celery to work

--- celery_app.py
+++ celery_app.py
@@ -1,4 +1,4 @@
 import shlex, subprocess, sys
 from subprocess import PIPE
-args = shlex.split(''' celery worker -A picha.celery -B -Q default -n default@%h -l info ''')
+args = shlex.split(''' celery -u default worker -A picha.celery -B -Q default -n default@%h -l info ''')
 stdout, stderr = subprocess.Popen(args, stdout=PIPE).communicate()
