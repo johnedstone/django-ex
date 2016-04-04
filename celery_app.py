d71be598430e4172fbfb37adf0a82cc384d7f679
Ed Johnstone <johnedstone@gmail.com>
2016-04-04 06:19:10 +0000

    working on templates

--- celery_app.py
+++ celery_app.py
@@ -1,6 +1,6 @@
 import shlex, subprocess, sys
 from subprocess import PIPE
-args = shlex.split(''' celery worker -A picha.celery -B -Q default -n default@%h -l info  ''')
+args = shlex.split(''' echo celery worker -A picha.celery -B -Q default -n default@%h -l info  ''')
 # stdout, stderr = subprocess.Popen(args, stdout=PIPE).communicate()
 try:
     p = subprocess.Popen(args)
