4150bf1870465bef42ed296496bfa4582ddc3c17
Ed Johnstone <johnedstone@gmail.com>
2016-04-07 01:51:10 +0000

    using openshift variables, not docker-compose variables for redis

--- picha/settings.py
+++ picha/settings.py
@@ -90,6 +90,28 @@ fh.write('REDIS_PORT:{}\n'.format(REDIS_PORT))
 
 REDIS_PORT_A = 6379
 REDIS_HOST_A = os.environ.get('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')
+REDIS_PORT = 6379
+REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')
+fh.write('REDIS_HOST_A:{}\n'.format(REDIS_HOST_A))
+fh.write('REDIS_PORT_A:{}\n'.format(REDIS_PORT_A))
+fh.close()
+
+# https://realpython.com/blog/python/asynchronous-tasks-with-django-and-celery/ Step 3
+BROKER_URL = 'redis://{host}:{port}'.format(host=REDIS_HOST,port=REDIS_PORT)
+CELERY_RESULT_BACKEND = 'redis://{host}:{port}'.format(host=REDIS_HOST,port=REDIS_PORT)
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
+
+CELERY_DEFAULT_QUEUE = 'default'
+CELERY_QUEUES = (
+    Queue('default', Exchange('default'), routing_key='default'),
+)
+
+## For later
+# REDIS_DB = 0
 fh.write('REDIS_HOST_A:{}\n'.format(REDIS_HOST_A))
 fh.write('REDIS_PORT_A:{}\n'.format(REDIS_PORT_A))
 fh.close()
