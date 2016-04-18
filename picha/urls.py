935fb67c449a0f17c2f5482739753eb42eba2f3b
Ed Johnstone <johnedstone@gmail.com>
2016-04-18 00:48:20 +0000

    working on v6: REST API

--- picha/urls.py
+++ picha/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from photos.views import PhotoView
+from bsub.urls import router
 from feedback.views import FeedbackView
+from photos.views import PhotoView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     url(r'^$', 'welcome.views.index', name='home'),
@@ -11,4 +13,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^experiences/', include('experiences.urls', namespace='experiences')),
     url(r'^feedback/$', FeedbackView.as_view(), name='feedback'),
+    url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'^api/', include(router.urls)),
 ]
